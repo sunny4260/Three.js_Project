87a4be471731ad66aa31fbe39ea9c6a7cbaad8c4
Sunny <kharwarsunny493@gmail.com>
2023-04-06 17:26:29 +0530

    Three.js_Project

---
+++ client/src/components/AIPicker.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import CustomButton from './CustomButton'
+
+const AIPicker = ({prompt, setPrompt, generatingImg, handleSubmit}) => {
+  return (
+    <div className='aipicker-container'>
+      <textarea
+       placeholder='Ask Rex....'
+       rows={5}
+       value={prompt}
+       onChange={(e) => setPrompt(e.target.value)}
+       className='aipicker-textarea'
+      />
+      <div className='flex flex-wrap gap-3'>
+        {generatingImg ? (
+          <CustomButton
+           type="outline"
+           title="Asking Rex..."
+           customStyle="text-xs"
+          />
+        ): (
+          <>
+            <CustomButton
+              type="outline"
+              title="AI Logo"
+              handleClick={() => handleSubmit('logo')}
+              customStyle="text-xs"
+            />
+            <CustomButton
+              type="filled"
+              title="AI Full"
+              handleClick={() => handleSubmit('full')}
+              customStyle="text-xs"
+            />
+          </>
+        )}
+
+      </div>
+    </div>
+  )
+}
+
+export default AIPicker
